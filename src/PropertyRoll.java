ce308562a1e2948803c8d3d1aac6f0cd1a197977
nils <nils.bohland@icloud.com>
2020-01-01 18:51:09 +0100

    PropertyRoll hat Bugs

--- src/PropertyRoll.java
+++ src/PropertyRoll.java
@@ -1,6 +1,37 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class PropertyRoll {
     public static int value(){
-        int diceValue = 0;
-        return diceValue;
+        return 1;
+    }
+
+    public static int rollAndSortOut(){
+        int lowestValue = new Dice(6).value();
+        int totalValue = 0;
+        for (int i = 3; i>0; i--){
+            int tmp = new Dice(6).value();
+                if (tmp > lowestValue)
+                    totalValue = totalValue + tmp;
+                if (tmp <= lowestValue) {
+                    totalValue = totalValue + lowestValue;
+                    lowestValue = tmp;
+                }
+        }
+        return totalValue;
+    }
+
+    public static Collection<Integer> propertyValues(){
+        ArrayList<Integer> propertyValues = new ArrayList<Integer>();
+        for (int i = 6; i > 0; i--)
+            propertyValues.add(rollAndSortOut());
+        return propertyValues;
+    }
+
+    public static void printPropertyValues(){
+        Collection<Integer> values = propertyValues();
+        for (Integer value: values){
+            System.out.println(value);
+        }
     }
 }
