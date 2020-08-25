52b0837580b76400c0d5727b98a9e188fac99b1f
nils <nils.bohland@icloud.com>
2020-08-25 10:40:59 +0200

    Design leicht verbessert, Initiativ-Liste in eine Scene gepackt.
    Theresa macht GUI Design.
    
    FileReader hinzufügen muss noch gemacht werden.
    
    Stand: 21.08.2020 - 12:20
    
    rest commited

--- src/GetFromFile.java
+++ src/GetFromFile.java
@@ -1,18 +1,27 @@
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class GetFromFile {
 
     public Collection<Fighter> getGroup(File f){
         ArrayList<Fighter> returnList = new ArrayList<Fighter>();
+        Collection<String> input= new HashSet<String>();
         try {
             Scanner s = new Scanner(f);
+            while (s.hasNextLine()){
+                input.add(s.nextLine());
+            }
         }   catch (FileNotFoundException e) { }
 
+        for (String s : input){
+            returnList.add(new Fighter(s.split(":")[0], s.split(":")[1]));
+        }
 
         return returnList;
     }
